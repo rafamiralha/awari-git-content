17c2f15a18357caad7bab3ac89d369ba5ed4b835
Rafael Miralha <rafamiralha1@gmail.com>
2024-05-07 07:46:53 -0300

    homework 05

--- form-04/script.js
+++ form-04/script.js
@@ -1,2 +1,12 @@
- let acaoClick;
+const form = document.getElementById('contact-form')
 
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    const nome = form.elements['name'].value;
+    const email = form.elements['email'].value;
+    const message = form.elements['message'].value;
+    const result = `Nome: ${nome} \n E-Mail:${email} \n Mensagem:${message}`
+    alert(result);
+
+
+});
