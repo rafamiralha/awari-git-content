04a9e01330ef85a1bd51977c661b3c52ef1075ac
Rafael Miralha <rafamiralha1@gmail.com>
2024-09-18 06:20:09 -0300

    Update ex10.js

--- Aula-6/javascript-challenge/ex10.js
+++ Aula-6/javascript-challenge/ex10.js
@@ -4,14 +4,18 @@ let square = '';
 function squareMaker(face1){
     for(let i = 0; i< face1;i++){
         for(let j = 0; j < face1;j++){
-              
+            for(let k = 0;k<face1;k++){
+                
+                column += row;
+            }
+        
+                
            
         }
-        column += row;
+        square+=column;
         
     }
-
-    return console.log(column)
+    return console.log(square)
     
 }
 squareMaker(5);
